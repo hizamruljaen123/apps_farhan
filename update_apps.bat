ee5f17013c52864fdfb2960f39115ecd6c167b36
unknown <hizamrulsite@gmail.com>
2024-06-23 18:41:37 +0700

    Pembaharuan Farhan

---
+++ update_apps.bat
@@ -0,0 +1,2 @@
+cd C:\Users\ASUS\Downloads\apps_farhan\apps_farhan
+git pull origin main
---
+++ upload.bat
@@ -0,0 +1,4 @@
+cd C:\Users\ASUS\Downloads\apps_farhan\apps_farhan
+git add .
+git commit -m "Pembaharuan Farhan"
+git push
